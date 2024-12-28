11ba906e13560d56ca321ed191c2ce1cfdc1f42b
shivamgupta2020 <shivam.gupta1968@gmail.com>
2024-12-28 12:51:11 +0530

    updated timer limit

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const PORT = 10000;
 app.use(express.static("public"));
 
 app.get("/run-script", async (req, res) => {
-    req.setTimeout(300000);
     try {
         const result = await scrapeTwitterTrends();
         if (result) {
